21b96f5220a4453ea54532984dc22b6a36f72749
Gabri126 <vizzusogabriele99@gmail.com>
2019-06-28 00:03:28 +0200

    updated DB generator and refresh

--- source/config/initDB/GeneratorSQL.sql
+++ source/config/initDB/GeneratorSQL.sql
@@ -1,25 +1,23 @@
-create table utenti
-(
-	nome varchar(30) not null,
-	cognome varchar(30) not null,
-	username varchar(30) primary key,
-	password varchar(30) not null,
-	email varchar(30) not null,
-	admin tinyint(1) default 0 not null
+CREATE TABLE `utenti` (
+    `nome` varchar(30) NOT NULL,
+    `cognome` varchar(30) NOT NULL,
+    `username` varchar(30) NOT NULL,
+    `password` varchar(30) NOT NULL,
+    `email` varchar(30) NOT NULL
 );
 
 CREATE TABLE `corsi` (
-  `materia` varchar(100) NOT NULL,
-  `docente1` varchar(100) NOT NULL,
-  `docente2` varchar(100) NOT NULL,
-  `nomecorso` varchar(100) NOT NULL,
-  `crediti` int(11) NOT NULL,
-  `descrizione` varchar(10000) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+    `materia` varchar(100) NOT NULL,
+    `docente1` varchar(100) NOT NULL,
+    `docente2` varchar(100) NOT NULL,
+    `nomecorso` varchar(100) NOT NULL,
+    `crediti` int(11) NOT NULL,
+    `descrizione` varchar(10000) NOT NULL
+);
 
 CREATE TABLE `admin` (
-  `nome` varchar(30) NOT NULL,
-  `cognome` varchar(30) NOT NULL,
-  `username` varchar(30) NOT NULL,
-  `password` varchar(30) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+    `nome` varchar(30) NOT NULL,
+    `cognome` varchar(30) NOT NULL,
+    `username` varchar(30) NOT NULL,
+    `password` varchar(30) NOT NULL
+);
--- source/config/initDB/RefreshDatabaseSQL.sql
+++ source/config/initDB/RefreshDatabaseSQL.sql
@@ -1,34 +1,20 @@
-Database utenti
-
 INSERT INTO utenti (nome, cognome, username, password, email)
-VALUES ('Simone', 'Vizzuso', 'Vizzu95', 'qwerty', 'simone@vizzuso.it');
-
+VALUES ('Simone', 'Vizzuso', 'user', 'user', 'simone@vizzuso.it');
 
 INSERT INTO utenti (nome, cognome, username, password, email, admin)
 VALUES ('Gabiele', 'Vizzuso', 'admin1', 'admin1', 'admin1@admin.it', 1);
 
-
 INSERT INTO utenti (nome, cognome, username, password, email, admin)
 VALUES ('Carlo', 'Valente', 'admin2', 'admin2', 'admin2@admin.it', 1);
 
-
-
-
-
-
-Database corsi
-
-INSERT INTO corsi (materia, docente1, docente2, nome, crediti, descrizione) 
+INSERT INTO `corsi`(`materia`, `docente1`, `docente2`, `nomecorso`, `crediti`, `descrizione`)
 VALUES('Informatica', 'Cristina Gena', '', 'Introduzione all\' informatica e alle reti', 9, 'che bel corso');
 
-INSERT INTO corsi (materia, docente1, docente2, nome, crediti, descrizione) 
+INSERT INTO `corsi`(`materia`, `docente1`, `docente2`, `nomecorso`, `crediti`, `descrizione`)
 VALUES('Informatica', 'Console Luca', '', 'Sviluppo softwere di applicazioni Informatiche', 12, 'aadasascvda');
-
-
-Database admin
 
 INSERT INTO `admin` (nome, cognome, username, password) 
 VALUES('Carlo Alberto', 'Valente', 'ca19', '12345');
 
 INSERT INTO `admin` (nome, cognome, username, password) 
-VALUES('Gabriele', 'Vizzuso', 'Gabri126', '12345');
+VALUES('Gabriele', 'Vizzuso', 'admin', 'admin');
