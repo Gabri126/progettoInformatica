b3ed2050b5f7428e10b6a88d7f2d7d7bcb798325
Gabri126 <vizzusogabriele99@gmail.com>
2019-06-28 00:05:18 +0200

    fixed DB refresh

--- source/config/initDB/RefreshDatabaseSQL.sql
+++ source/config/initDB/RefreshDatabaseSQL.sql
@@ -1,11 +1,11 @@
-INSERT INTO utenti (nome, cognome, username, password, email)
+INSERT INTO `utenti`(`nome`, `cognome`, `username`, `password`, `email`)
 VALUES ('Simone', 'Vizzuso', 'user', 'user', 'simone@vizzuso.it');
 
-INSERT INTO utenti (nome, cognome, username, password, email, admin)
-VALUES ('Gabiele', 'Vizzuso', 'admin1', 'admin1', 'admin1@admin.it', 1);
+INSERT INTO `utenti`(`nome`, `cognome`, `username`, `password`, `email`)
+VALUES ('Gabiele', 'Vizzuso', 'admin1', 'admin1', 'admin1@admin.it');
 
-INSERT INTO utenti (nome, cognome, username, password, email, admin)
-VALUES ('Carlo', 'Valente', 'admin2', 'admin2', 'admin2@admin.it', 1);
+INSERT INTO `utenti`(`nome`, `cognome`, `username`, `password`, `email`)
+VALUES ('Carlo', 'Valente', 'admin2', 'admin2', 'admin2@admin.it');
 
 INSERT INTO `corsi`(`materia`, `docente1`, `docente2`, `nomecorso`, `crediti`, `descrizione`)
 VALUES('Informatica', 'Cristina Gena', '', 'Introduzione all\' informatica e alle reti', 9, 'che bel corso');
@@ -13,8 +13,8 @@ VALUES('Informatica', 'Cristina Gena', '', 'Introduzione all\' informatica e all
 INSERT INTO `corsi`(`materia`, `docente1`, `docente2`, `nomecorso`, `crediti`, `descrizione`)
 VALUES('Informatica', 'Console Luca', '', 'Sviluppo softwere di applicazioni Informatiche', 12, 'aadasascvda');
 
-INSERT INTO `admin` (nome, cognome, username, password) 
+INSERT INTO `admin`(`nome`, `cognome`, `username`, `password`)
 VALUES('Carlo Alberto', 'Valente', 'ca19', '12345');
 
-INSERT INTO `admin` (nome, cognome, username, password) 
+INSERT INTO `admin`(`nome`, `cognome`, `username`, `password`)
 VALUES('Gabriele', 'Vizzuso', 'admin', 'admin');
